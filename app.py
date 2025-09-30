48eee8092ec4071be966a10c876d5ca31237e5bd
Björn Otto <bjoern.otto@ovgu.de>
2025-09-30 11:32:48 +0200

    Bind to 0.0.0.0

--- app.py
+++ app.py
@@ -1139,4 +1139,4 @@ def chat():
 
 if __name__ == '__main__':
     print("Starting Flask server")
-    app.run(debug=False, host='127.0.0.1', port=5000)
+    app.run(debug=False, host='0.0.0.0', port=5000)
